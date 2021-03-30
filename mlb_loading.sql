ad0a8a0e3f3fae07420da46c272fdb4ce1a6b0c9
Rhea Mehta <Rkmehta@uwaterloo.ca>
2021-03-30 00:30:43 -0400

    loaded umpire tables and gamebatterstats table

--- mlb_loading.sql
+++ mlb_loading.sql
@@ -4,6 +4,9 @@ drop table if exists AtBats;
 drop table if exists Pitches;
 drop table if exists Ejections;
 drop table if exists Games;
+drop table if exists Umpires;
+drop table if exists GameUmpireStats;
+drop table if exists GameBatterStats;
 -- Players -------------------------------------------------------------------
 select '----------------------------------------------------------------' as '';
 select 'Create Players' as '';
@@ -37,7 +40,10 @@ create table AtBats (ab_id decimal(10),
 			stand char(1),
 			top char(5),
 		-- Constraints	    
-			primary key (ab_id)
+			primary key (ab_id),
+			check (p_throws = 'L' or p_throws = 'R'),
+			check (stand = 'L' or stand = 'R'),
+			check (top = 'True' or top = 'False')
 		);
 
 load data infile '/var/lib/mysql-files/MLB/atbats.csv' ignore into table AtBats
@@ -46,7 +52,7 @@ load data infile '/var/lib/mysql-files/MLB/atbats.csv' ignore into table AtBats
      ignore 1 lines;
 
 
--- At Bat -------------------------------------------------------------------
+-- Pitches -------------------------------------------------------------------
 select '----------------------------------------------------------------' as '';
 select 'Create Pitches' as '';
 
@@ -158,20 +164,6 @@ load data infile '/var/lib/mysql-files/MLB/ejections.csv' ignore into table Ejec
      ignore 1 lines
 	(ab_id, des, event_num, g_id, player_id, @throwaway, bs, correct, team, is_home_team);
 
--- Umpires -------------------------------------------------------------------
-select '----------------------------------------------------------------' as '';
-select 'Create Umpires' as '';
-
--- Umpire Stats-------------------------------------------------------------------
-select '----------------------------------------------------------------' as '';
-select 'Create UmpireStats' as '';
-
--- Game Batter Stats-------------------------------------------------------------------
-select '----------------------------------------------------------------' as '';
-select 'Create GameBatterStats' as '';
-
-
-
 select 'Create Games' as '';
 drop table Games;
 create table Games (attendance decimal(5) check (attendance >= 0),
@@ -202,3 +194,108 @@ load data infile '/var/lib/mysql-files/MLB/games.csv' ignore into table Games
      lines terminated by '\n'
      ignore 1 lines
 	(attendance,away_final_score,away_team,date,elapsed_time,g_id,home_final_score,home_team,start_time,umpire_1B,umpire_2B,umpire_3B,umpire_HP,venue_name,weather,wind,delay);
+
+-- Umpires -------------------------------------------------------------------
+select '----------------------------------------------------------------' as '';
+select 'Create Umpires' as '';
+create table Umpires (umpire_id int not null auto_increment,
+			first_name char(20) not null,
+			last_name char(20) not null,
+-- Constraints	    
+		    primary key (umpire_id)
+		);
+insert into Umpires (first_name, last_name)
+	(select substring(umpire_1B, 1, locate(' ', umpire_1B)), substring(umpire_1B, locate(' ', umpire_1B) + 1) from Games group by umpire_1B);
+
+insert into Umpires (first_name, last_name)
+	(select substring(umpire_2B, 1, locate(' ', umpire_2B)), substring(umpire_2B, locate(' ', umpire_2B) + 1) from Games 
+	where not exists (select first_name, last_name from Umpires) group by umpire_2B);
+
+insert into Umpires (first_name, last_name)
+	(select substring(umpire_3B, 1, locate(' ', umpire_3B)), substring(umpire_3B, locate(' ', umpire_3B) + 1) from Games 
+	where not exists (select first_name, last_name from Umpires) group by umpire_3B);
+
+insert into Umpires (first_name, last_name)
+	(select substring(umpire_HP, 1, locate(' ', umpire_HP)), substring(umpire_HP, locate(' ', umpire_HP) + 1) from Games 
+	where not exists (select first_name, last_name from Umpires) group by umpire_HP);
+
+-- Umpire Stats-------------------------------------------------------------------
+select '----------------------------------------------------------------' as '';
+select 'Create GameUmpireStats' as '';
+
+create table GameUmpireStats (umpire_id int,
+			g_id decimal(9),
+			position char(2),
+			ejections decimal(2),
+			bs decimal(2),
+			bs_correct decimal(2),
+            primary key(g_id, umpire_id)
+		-- Constraints	    
+		);
+
+insert into GameUmpireStats (umpire_id, g_id, position, ejections, bs, bs_correct)
+    (select distinct Umpires.umpire_id, Games.g_id, '1B', 0, 0, 0 from Umpires 
+    inner join Games on Games.umpire_1B = concat(Umpires.first_name, ' ', Umpires.last_name));
+
+insert into GameUmpireStats (umpire_id, g_id, position, ejections, bs, bs_correct)
+    (select distinct Umpires.umpire_id, Games.g_id, '2B', 0, 0, 0 from Umpires 
+    inner join Games on Games.umpire_2B = concat(Umpires.first_name, ' ', Umpires.last_name));
+
+insert into GameUmpireStats (umpire_id, g_id, position, ejections, bs, bs_correct)
+    (select distinct Umpires.umpire_id, Games.g_id, '3B', 0, 0, 0 from Umpires 
+    inner join Games on Games.umpire_3B = concat(Umpires.first_name, ' ', Umpires.last_name));
+
+insert into GameUmpireStats (umpire_id, g_id, position, ejections, bs, bs_correct)
+    (select distinct Umpires.umpire_id, Games.g_id, 'HP', 0, 0, 0 from Umpires 
+    inner join Games on Games.umpire_HP = concat(Umpires.first_name, ' ', Umpires.last_name));
+
+with num_ejections as
+(select g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2) as position, count(*) as count from Ejections group by g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2))
+update GameUmpireStats
+    inner join num_ejections on GameUmpireStats.g_id = num_ejections.g_id 
+    and GameUmpireStats.position = num_ejections.position
+    set GameUmpireStats.ejections = num_ejections.count;
+
+with num_bs as
+(select g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2) as position, count(*) as count from Ejections where Ejections.bs = 'Y' group by g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2))
+update GameUmpireStats
+    inner join num_bs on GameUmpireStats.g_id = num_bs.g_id 
+    and GameUmpireStats.position = num_bs.position
+    set GameUmpireStats.bs = num_bs.count;
+
+with num_correct as
+(select g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2) as position, count(*) as count from Ejections where Ejections.bs = 'Y' and Ejections.correct = 'C' group by g_id, substring(Ejections.des, locate('by ', Ejections.des) + 3, 2))
+update GameUmpireStats
+    inner join num_correct on GameUmpireStats.g_id = num_correct.g_id 
+    and GameUmpireStats.position = num_correct.position
+    set GameUmpireStats.bs_correct = num_correct.count;
+
+-- Game Batter Stats-------------------------------------------------------------------
+select '----------------------------------------------------------------' as '';
+select 'Create GameBatterStats' as '';
+
+create table GameBatterStats (batter_id decimal(6),
+			g_id decimal(9),
+			team_id char(3),
+			outs decimal(2),
+			stand char(1),
+            primary key(g_id, batter_id)
+		-- Constraints	    
+		);
+
+insert into GameBatterStats(batter_id, g_id, outs, stand)
+	(select batter_id, g_id, sum(o) from AtBats group by batter_id, g_id);
+
+update GameBatterStats
+	inner join AtBats on AtBats.g_id = GameBatterStats.g_id and AtBats.batter_id = GameBatterStats.batter_id
+	set GameBatterStats.stand = AtBats.stand;
+
+update GameBatterStats
+	inner join AtBats on AtBats.g_id = GameBatterStats.g_id and AtBats.batter_id = GameBatterStats.batter_id
+	inner join Games on Games.g_id = AtBats.g_id
+	set GameBatterStats.team_id = Games.away_team where AtBats.top = 'True';
+
+update GameBatterStats
+	inner join AtBats on AtBats.g_id = GameBatterStats.g_id and AtBats.batter_id = GameBatterStats.batter_id
+	inner join Games on Games.g_id = AtBats.g_id
+	set GameBatterStats.team_id = Games.home_team where AtBats.top = 'False';
