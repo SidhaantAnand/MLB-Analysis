32c07e610ce7b8adffc1aef571fd4e1c4127ed3e
SIDHAANT ANAND <S29ANAND@uwaterloo.ca>
2021-03-29 21:02:57 -0400

    Games added

--- mlb_loading.sql
+++ mlb_loading.sql
@@ -1,8 +1,9 @@
+warnings;
 drop table if exists Players;
 drop table if exists AtBats;
 drop table if exists Pitches;
 drop table if exists Ejections;
-
+drop table if exists Games;
 -- Players -------------------------------------------------------------------
 select '----------------------------------------------------------------' as '';
 select 'Create Players' as '';
@@ -168,3 +169,36 @@ select 'Create UmpireStats' as '';
 -- Game Batter Stats-------------------------------------------------------------------
 select '----------------------------------------------------------------' as '';
 select 'Create GameBatterStats' as '';
+
+
+
+select 'Create Games' as '';
+drop table Games;
+create table Games (attendance decimal(5) check (attendance >= 0),
+			away_final_score decimal(2) not null check (away_final_score >= 0),
+			away_team char(3) not null,
+			date datetime,
+			elapsed_time decimal(3),
+			g_id decimal(9) not null,
+			home_final_score decimal(2) not null check (home_final_score >= 0),
+			home_team char(3) not null,
+			start_time varchar(10) not null check (start_time REGEXP '[0-9]:[0-9][0-9] [A|P][M]'),
+			umpire_1B varchar(300),
+			umpire_2B varchar(300),
+			umpire_3B varchar(300),
+			umpire_HP varchar(300),
+			venue_name varchar(300),
+			weather varchar(300) not null check (weather REGEXP '[0-9]+ degrees, (clear|sunny|overcast|cloudy|partly cloudy|snow|drizzle|dome|roof closed|rain)'),
+			wind varchar(300) not null check (wind REGEXP '[0-9]+ mph,.*') ,
+			delay varchar(300),
+
+			primary key (g_id)
+		);
+
+
+load data infile '/var/lib/mysql-files/MLB/games.csv' ignore into table Games
+     fields terminated by ','
+     OPTIONALLY ENCLOSED BY '"'
+     lines terminated by '\n'
+     ignore 1 lines
+	(attendance,away_final_score,away_team,date,elapsed_time,g_id,home_final_score,home_team,start_time,umpire_1B,umpire_2B,umpire_3B,umpire_HP,venue_name,weather,wind,delay);
