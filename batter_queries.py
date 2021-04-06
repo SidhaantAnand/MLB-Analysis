85d0f18e492dfdf652fec2a7a219c1a379a2a04b
Rhea Mehta <Rkmehta@uwaterloo.ca>
2021-04-06 18:32:27 -0400

    changed the queries to be functions to call

--- batter_queries.py
+++ batter_queries.py
@@ -1,126 +1,169 @@
-# place holder values - should be set by the client application
-player_name = "Matt Carpenter"
-player_name_split = player_name.split()
-
-# sql queries
 # (num_ejections)
-player_ejections_sql = f"""with playerId as
-    (select player_id from Players where 
-    first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-    select count(*) as num_ejections from Ejections
-    inner join AtBats using (ab_id)
-    inner join playerId on AtBats.batter_id =  playerId.player_id;"""
+def player_ejections(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select count(*) as num_ejections from Ejections
+        inner join AtBats using (ab_id)
+        inner join playerId on AtBats.batter_id =  playerId.player_id;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (num_times_out, num_hits, num_home_runs)
-player_hits_outs_sql = f"""with playerId as
-(select player_id from Players where 
-first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-select sum(outs) as num_outs, sum(hits) as num_hits, sum(home_runs) as num_home_runs from GameBatterStats
-inner join playerId on playerId.player_id = GameBatterStats.batter_id
-where batter_id = playerId.player_id;"""
+def player_hits_outs(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select sum(outs) as num_outs, sum(hits) as num_hits, sum(home_runs) as num_home_runs from GameBatterStats
+        inner join playerId on playerId.player_id = GameBatterStats.batter_id
+        where batter_id = playerId.player_id;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (most_played_inning, num_times_played_inning)
-player_inning_sql = f"""with playerId as
-    (select player_id from Players where 
-    first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-    select inning, count(inning) from AtBats
-    inner join playerId on AtBats.batter_id =  playerId.player_id
-    group by inning
-    order by count(inning) desc
-    limit 1;"""
+def player_inning(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select inning, count(inning) from AtBats
+        inner join playerId on AtBats.batter_id =  playerId.player_id
+        group by inning
+        order by count(inning) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (prefered_side - L or R)
-player_stand_sql = f"""with playerId as
-    (select player_id from Players where 
-    first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-    select stand from AtBats
-    inner join playerId on AtBats.batter_id =  playerId.player_id
-    group by stand
-    order by count(stand) desc
-    limit 1;"""
+def player_stand(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select stand from AtBats
+        inner join playerId on AtBats.batter_id =  playerId.player_id
+        group by stand
+        order by count(stand) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (best pitch type - w the least number of strikes)
-player_best_pitch_type_sql = f"""with playerId as
-    (select player_id from Players where 
-    first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-    select pitch_type from Pitches
-    inner join AtBats on AtBats.ab_id = Pitches.ab_id
-    inner join playerId on AtBats.batter_id =  playerId.player_id
-    where code = 'B' or '*B'
-    group by pitch_type
-    order by count(pitch_type)/sum(b_count) desc
-    limit 1;"""
+def player_best_pitch_type(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select pitch_type from Pitches
+        inner join AtBats on AtBats.ab_id = Pitches.ab_id
+        inner join playerId on AtBats.batter_id =  playerId.player_id
+        where code = 'B' or '*B'
+        group by pitch_type
+        order by count(pitch_type)/sum(b_count) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (worst pitch type - w the most number of strikes)
-player_worst_pitch_type_sql = f"""with playerId as
-    (select player_id from Players where 
-    first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
-    select pitch_type from Pitches
-    inner join AtBats on AtBats.ab_id = Pitches.ab_id
-    inner join playerId on AtBats.batter_id =  playerId.player_id
-    where code != 'B' and code != '*B'
-    group by pitch_type
-    order by count(pitch_type)/sum(b_count) desc
-    limit 1;"""
+def player_worst_pitch_type(mycursor, player_name):
+    player_name_split = player_name.split()
+    sql = f"""with playerId as
+        (select player_id from Players where 
+        first_name = '{player_name_split[0]}' and last_name = '{player_name_split[1]}')
+        select pitch_type from Pitches
+        inner join AtBats on AtBats.ab_id = Pitches.ab_id
+        inner join playerId on AtBats.batter_id =  playerId.player_id
+        where code != 'B' and code != '*B'
+        group by pitch_type
+        order by count(pitch_type)/sum(b_count) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (best batter - balls:strikeout ratio)
-best_batter_bsratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join AtBats on AtBats.batter_id =  playerId.player_id
-    inner join Pitches on AtBats.ab_id = Pitches.ab_id
-    group by full_name
-    order by sum(b_count)/sum(s_count) desc
-    limit 1;"""
+def best_batter_bsratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join AtBats on AtBats.batter_id =  playerId.player_id
+        inner join Pitches on AtBats.ab_id = Pitches.ab_id
+        group by full_name
+        order by sum(b_count)/sum(s_count) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (worst batter - balls:strikeout ratio)
-worst_batter_bsratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join AtBats on AtBats.batter_id =  playerId.player_id
-    inner join Pitches on AtBats.ab_id = Pitches.ab_id
-    group by full_name
-    order by sum(b_count)/sum(s_count)
-    limit 1;"""
+def worst_batter_bsratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join AtBats on AtBats.batter_id =  playerId.player_id
+        inner join Pitches on AtBats.ab_id = Pitches.ab_id
+        group by full_name
+        order by sum(b_count)/sum(s_count)
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (best batter - hits:games ratio)
-best_batter_hgratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
-    group by full_name
-    order by sum(GameBatterStats.hits)/count(GameBatterStats.hits) desc
-    limit 1;"""
+def best_batter_hgratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
+        group by full_name
+        order by sum(GameBatterStats.hits)/count(GameBatterStats.hits) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (worst batter - hits:games ratio)
-worst_batter_hgratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
-    group by full_name
-    order by sum(GameBatterStats.hits)/count(GameBatterStats.hits)
-    limit 1;"""
+def worst_batter_hgratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
+        group by full_name
+        order by sum(GameBatterStats.hits)/count(GameBatterStats.hits)
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (best batter - home_runs:games ratio)
-best_batter_hrgratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
-    group by full_name
-    order by sum(GameBatterStats.home_runs)/count(GameBatterStats.home_runs) desc
-    limit 1;"""
+def best_batter_hrgratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
+        group by full_name
+        order by sum(GameBatterStats.home_runs)/count(GameBatterStats.home_runs) desc
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 # (worst batter - home_runs:games ratio)
-worst_batter_hrgratio_sql = f"""with playerId as
-    (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
-    select full_name from playerId
-    inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
-    group by full_name
-    order by sum(GameBatterStats.home_runs)/count(GameBatterStats.home_runs)
-    limit 1;"""
-
-# printing results - can print whatever query, more for the client side to handle
-mycursor.execute(player_hits_outs_sql)
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
+def worst_batter_hrgratio(mycursor):
+    sql = f"""with playerId as
+        (select player_id, concat(first_name, ' ', last_name) as full_name from Players)
+        select full_name from playerId
+        inner join GameBatterStats on GameBatterStats.batter_id =  playerId.player_id
+        group by full_name
+        order by sum(GameBatterStats.home_runs)/count(GameBatterStats.home_runs)
+        limit 1;"""
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
--- umpire_queries.py
+++ umpire_queries.py
@@ -1,65 +1,68 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="99.250.146.93",
-  user="root",
-  password="MLB_Gang",
-  database="MLB"
-)
-
-mycursor = mydb.cursor()
-
-# place holder values - should be set by the client application
-umpire_name = "Mike Everitt"
-
-# sql queries
 # (num_games)
-umpire_games_sql = f"""with umpireId as
-(select umpire_id from Umpires where full_name = '{umpire_name}')
-select count(*) as num_games from GameUmpireStats
-inner join umpireId using (umpire_id);"""
+def umpire_games(mycursor, umpire_name):
+  sql = f"""with umpireId as
+    (select umpire_id from Umpires where full_name = '{umpire_name}')
+    select count(*) as num_games from GameUmpireStats
+    inner join umpireId using (umpire_id);"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult  
 
 # (num_outs, num_challenges, num_correct_challenges)
-umpire_ejections_sql = f"""with umpireId as
-(select umpire_id from Umpires where full_name = '{umpire_name}')
-select sum(ejections) as num_outs, sum(bs) as num_challenges, sum(bs_correct) as num_correct_challenges from GameUmpireStats
-inner join umpireId using (umpire_id);"""
+def umpire_ejections(mycursor, umpire_name):
+  sql = f"""with umpireId as
+    (select umpire_id from Umpires where full_name = '{umpire_name}')
+    select sum(ejections) as num_outs, sum(bs) as num_challenges, sum(bs_correct) as num_correct_challenges from GameUmpireStats
+    inner join umpireId using (umpire_id);"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult  
 
 # (usual position)
-umpire_position_sql = f"""with umpireId as
-(select umpire_id from Umpires where full_name = '{umpire_name}')
-select position from GameUmpireStats
-inner join umpireId using (umpire_id)
-group by position
-order by count(position) desc limit 1;"""
+def umpire_position(mycursor, umpire_name):
+  sql = f"""with umpireId as
+    (select umpire_id from Umpires where full_name = '{umpire_name}')
+    select position from GameUmpireStats
+    inner join umpireId using (umpire_id)
+    group by position
+    order by count(position) desc limit 1;"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult
 
 # (usual venue)
-umpire_venue_sql = f"""with umpireId as
-(select umpire_id from Umpires where full_name = '{umpire_name}')
-select venue_name from Games
-inner join GameUmpireStats using (g_id)
-inner join umpireId using (umpire_id)
-group by venue_name
-order by count(venue_name) desc limit 1;"""
+def umpire_venue(mycursor, umpire_name):
+  sql = f"""with umpireId as
+    (select umpire_id from Umpires where full_name = '{umpire_name}')
+    select venue_name from Games
+    inner join GameUmpireStats using (g_id)
+    inner join umpireId using (umpire_id)
+    group by venue_name
+    order by count(venue_name) desc limit 1;"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult
 
 # (best umpire)
-best_umpire_sql = f"""with umpireId as
-(select umpire_id, full_name from Umpires)
-select full_name from GameUmpireStats
-inner join umpireId using (umpire_id)
-group by full_name
-order by sum(ejections)/sum(bs) desc limit 1;"""
+def best_umpire(mycursor):
+  sql = f"""with umpireId as
+    (select umpire_id, full_name from Umpires)
+    select full_name from GameUmpireStats
+    inner join umpireId using (umpire_id)
+    group by full_name
+    order by sum(ejections)/sum(bs) desc limit 1;"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult
 
 # (worst umpire)
-worst_umpire_sql = f"""with umpireId as
-(select umpire_id, full_name from Umpires)
-select full_name from GameUmpireStats
-inner join umpireId using (umpire_id)
-group by full_name
-order by sum(ejections)/sum(bs) limit 1;"""
-
-# printing results - can print whatever query, more for the client side to handle
-mycursor.execute(worst_umpire_sql)
-myresult = mycursor.fetchall()
-for x in myresult:
-    print(x)
+def worst_umpire(mycursor):
+  sql = f"""with umpireId as
+    (select umpire_id, full_name from Umpires)
+    select full_name from GameUmpireStats
+    inner join umpireId using (umpire_id)
+    group by full_name
+    order by sum(ejections)/sum(bs) limit 1;"""
+  mycursor.execute(sql)
+  myresult = mycursor.fetchall()
+  return myresult
