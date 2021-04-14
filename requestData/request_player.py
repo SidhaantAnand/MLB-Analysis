cb531099dafaf5c24199aaddec9689280ac0a232
ShauryaMdn <shaurya8990@gmail.com>
2021-04-13 21:45:30 -0400

    Now can use players with duplicate names

--- requestData/request_player.py
+++ requestData/request_player.py
@@ -6,8 +6,10 @@ def request_player(cursor):
     result = cursor.fetchall()
     for x in result:
         if players.get(x[1]):
-            duplicate_player_names[x[1]] = x[0]
+            duplicate_player_names[x[1]] = [players[x[1]], x[0]]
             players.pop(x[1])
+        elif duplicate_player_names.get(x[1]):
+            duplicate_player_names.get(x[1]).append(x[0])
         else:
             players[x[1]] = x[0]
 
@@ -16,10 +18,20 @@ def request_player(cursor):
         if option in players:
             return [option, players[option]]
         elif option in duplicate_player_names:
-            print("Multiple player have that name")
+            print("Multiple players named " + option)
+            i = 1
+            for ID in duplicate_player_names[option]:
+                print(str(i) + '. Player ID:' + str(ID))
+                i += 1
+            player_id = input('Choose which player you want: ')
+            try:
+                player_id = int(player_id)
+            except ValueError:
+                print('Not a valid number')
+                continue
+            if player_id < 1 or player_id > i:
+                print('Invalid option')
+                continue
+            return [option, duplicate_player_names[option][player_id-1]]
         else:
             print("No such player found")
-
-
-def get_player_teams(cursor):
-    cursor.execute()
